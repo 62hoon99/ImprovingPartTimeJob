50e29f4ff8911b0244d5f7fd344b0f5a63ac2662
62hoon99 <98267772+62hoon99@users.noreply.github.com>
2022-12-24 10:58:51 +0900

    feat: 기본세팅 완료

---
+++ src/test/java/personal/ImprovingPartTimeJob/ImprovingPartTimeJobApplicationTests.java
@@ -0,0 +1,13 @@
+package personal.ImprovingPartTimeJob;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ImprovingPartTimeJobApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
