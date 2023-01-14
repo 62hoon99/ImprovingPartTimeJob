5621c0c70a203c152aee53d6e2839ead082a39d3
62hoon99 <98267772+62hoon99@users.noreply.github.com>
2023-01-14 21:29:55 +0900

    refactor: service repository 역할 분담 변경

--- src/main/java/personal/ImprovingPartTimeJob/repository/Repository.java
+++ src/main/java/personal/ImprovingPartTimeJob/repository/Repository.java
@@ -10,8 +10,6 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.*;
-import java.util.ArrayList;
-import java.util.List;
 import java.util.Objects;
 import java.util.UUID;
 
@@ -35,13 +33,7 @@ public class Repository {
     @Value("${spring.fixFile-dir}")
     private String fixFileDir;
 
-    public String modifyOrderFile(MultipartFile file, String password) throws IOException {
-        String storedFileName = saveFile(file);
-        saveModifiedOrderFile(storedFileName, password);
-        return storedFileName;
-    }
-
-    private void saveModifiedOrderFile(String storedFileName, String password) throws IOException {
+    public void saveModifiedOrderFile(String storedFileName, String password) throws IOException {
         Workbook workbook = WorkbookFactory.create(new File(getFileDir() + storedFileName), password);
         Sheet sheet = workbook.getSheetAt(0);
         sheet.shiftRows(1, sheet.getLastRowNum(), -1);
@@ -51,48 +43,15 @@ public class Repository {
         workbook.close();
     }
 
-    public String createBatchFile(MultipartFile orderFile, MultipartFile receiptFile) throws IOException, IllegalArgumentException {
-        String orderFileName = saveFile(orderFile);
-        String receiptFileName = saveFile(receiptFile);
-
-        Workbook wbBatchFile = WorkbookFactory.create(new File(getFixFileDir() + uploadFileName));
-        Workbook wbOrderFile = WorkbookFactory.create(new File(getFileDir() + orderFileName));
-        Workbook wbReceiptFile = WorkbookFactory.create(new File(getFileDir() + receiptFileName));
-
-        Sheet sheetBatch = wbBatchFile.getSheetAt(0);
-        Sheet sheetOrder = wbOrderFile.getSheetAt(0);
-        Sheet sheetReceipt = wbReceiptFile.getSheetAt(0);
-
-        List<String> orderNumber = getColumnValues(sheetOrder, 0);
-        List<String> waybillNumber = getColumnValues(sheetReceipt, 7);// H열
-        List<String> payeeName = getColumnValues(sheetReceipt, 12); // M열
-
-        setBatchFileValues(sheetBatch, orderNumber, waybillNumber, payeeName);
-
-        String storedBatchFileName = getRandomStoredFileName();
-        FileOutputStream fileOutputStream = new FileOutputStream(getFileDir() + storedBatchFileName);
-        wbBatchFile.write(fileOutputStream);
-        fileOutputStream.close();
-
-        wbBatchFile.close();
-        wbOrderFile.close();
-        wbReceiptFile.close();
-
-        deleteFile(orderFileName);
-        deleteFile(receiptFileName);
-
-        return storedBatchFileName;
-    }
-
     public void cleanupDirectory() {
         File file = new File(getFileDir());
 
-        for (String fileName : file.list()) {
+        for (String fileName : Objects.requireNonNull(file.list())) {
             deleteFile(fileName);
         }
     }
 
-    private String saveFile(MultipartFile file) throws IOException {
+    public String saveFile(MultipartFile file) throws IOException {
         String storedFileName = getRandomStoredFileName(extractExt(Objects.requireNonNull(file.getOriginalFilename())));
         file.transferTo(new File(getFileDir() + storedFileName));
         return storedFileName;
@@ -100,29 +59,6 @@ public class Repository {
 
     private String getRandomStoredFileName(String ext) {
         return UUID.randomUUID() + ext;
-    }
-
-    private String getRandomStoredFileName() {
-        return UUID.randomUUID() + ".xls";
-    }
-
-    private List<String> getColumnValues(Sheet sheetOrder, int index) {
-        List<String> valueList = new ArrayList<>();
-        for(int i = 1; i <= sheetOrder.getLastRowNum(); i++) {
-            valueList.add(sheetOrder.getRow(i).getCell(index).getStringCellValue());
-        }
-        return valueList;
-    }
-
-    private void setBatchFileValues(Sheet sheetUpload, List<String> orderNumber, List<String> waybillNumber, List<String> recieverName) {
-        for(int i = 0; i < orderNumber.size(); i++) {
-            Row row = sheetUpload.createRow(i + 1);
-            row.createCell(0).setCellValue(orderNumber.get(i));
-            row.createCell(1).setCellValue(shippingMethod);
-            row.createCell(2).setCellValue(shippingCompany);
-            row.createCell(3).setCellValue(waybillNumber.get(i));
-            row.createCell(4).setCellValue(recieverName.get(i));
-        }
     }
 
     private String extractExt(String originalFilename) {
@@ -136,6 +72,18 @@ public class Repository {
 
     public String getFixFileDir() {
         return System.getProperty("user.dir") + fixFileDir;
+    }
+
+    public String getUploadFileName() {
+        return uploadFileName;
+    }
+
+    public String getShippingMethod() {
+        return shippingMethod;
+    }
+
+    public String getShippingCompany() {
+        return shippingCompany;
     }
 
     public void deleteFile(String storedFileName) {
--- src/main/java/personal/ImprovingPartTimeJob/service/Service.java
+++ src/main/java/personal/ImprovingPartTimeJob/service/Service.java
@@ -1,10 +1,19 @@
 package personal.ImprovingPartTimeJob.service;
 
 import lombok.RequiredArgsConstructor;
+import org.apache.poi.ss.usermodel.Row;
+import org.apache.poi.ss.usermodel.Sheet;
+import org.apache.poi.ss.usermodel.Workbook;
+import org.apache.poi.ss.usermodel.WorkbookFactory;
 import org.springframework.web.multipart.MultipartFile;
 import personal.ImprovingPartTimeJob.repository.Repository;
 
+import java.io.File;
+import java.io.FileOutputStream;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.UUID;
 
 @org.springframework.stereotype.Service
 @RequiredArgsConstructor
@@ -12,7 +21,9 @@ public class Service {
     private final Repository repository;
 
     public String modifyOrderFile(MultipartFile file, String password) throws IOException {
-        return repository.modifyOrderFile(file, password);
+        String storedFileName = repository.saveFile(file);
+        repository.saveModifiedOrderFile(storedFileName, password);
+        return storedFileName;
     }
 
     public String getFileDir() {
@@ -24,7 +35,59 @@ public class Service {
     }
 
     public String createBatchFile(MultipartFile orderFile, MultipartFile receiptFile) throws IOException {
-        return repository.createBatchFile(orderFile, receiptFile);
+        String orderFileName = repository.saveFile(orderFile);
+        String receiptFileName = repository.saveFile(receiptFile);
+
+        Workbook wbBatchFile = WorkbookFactory.create(new File(repository.getFixFileDir() + repository.getUploadFileName()));
+        Workbook wbOrderFile = WorkbookFactory.create(new File(repository.getFileDir() + orderFileName));
+        Workbook wbReceiptFile = WorkbookFactory.create(new File(repository.getFileDir() + receiptFileName));
+
+        Sheet sheetBatch = wbBatchFile.getSheetAt(0);
+        Sheet sheetOrder = wbOrderFile.getSheetAt(0);
+        Sheet sheetReceipt = wbReceiptFile.getSheetAt(0);
+
+        List<String> orderNumber = getColumnValues(sheetOrder, 0);
+        List<String> waybillNumber = getColumnValues(sheetReceipt, 7);// H열
+        List<String> payeeName = getColumnValues(sheetReceipt, 12); // M열
+
+        setBatchFileValues(sheetBatch, orderNumber, waybillNumber, payeeName);
+
+        String storedBatchFileName = getRandomStoredFileName();
+        FileOutputStream fileOutputStream = new FileOutputStream(getFileDir() + storedBatchFileName);
+        wbBatchFile.write(fileOutputStream);
+        fileOutputStream.close();
+
+        wbBatchFile.close();
+        wbOrderFile.close();
+        wbReceiptFile.close();
+
+        deleteFile(orderFileName);
+        deleteFile(receiptFileName);
+
+        return storedBatchFileName;
+    }
+
+    private List<String> getColumnValues(Sheet sheetOrder, int index) {
+        List<String> valueList = new ArrayList<>();
+        for(int i = 1; i <= sheetOrder.getLastRowNum(); i++) {
+            valueList.add(sheetOrder.getRow(i).getCell(index).getStringCellValue());
+        }
+        return valueList;
+    }
+
+    private void setBatchFileValues(Sheet sheetUpload, List<String> orderNumber, List<String> waybillNumber, List<String> recieverName) {
+        for(int i = 0; i < orderNumber.size(); i++) {
+            Row row = sheetUpload.createRow(i + 1);
+            row.createCell(0).setCellValue(orderNumber.get(i));
+            row.createCell(1).setCellValue(repository.getShippingMethod());
+            row.createCell(2).setCellValue(repository.getShippingCompany());
+            row.createCell(3).setCellValue(waybillNumber.get(i));
+            row.createCell(4).setCellValue(recieverName.get(i));
+        }
+    }
+
+    private String getRandomStoredFileName() {
+        return UUID.randomUUID() + ".xls";
     }
 
     public void cleanUpDirectory() {
